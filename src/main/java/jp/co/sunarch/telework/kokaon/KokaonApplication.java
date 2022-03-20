d9a3d378ca46038ac718d84d4f59647c6cb58127
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2022-03-20 22:52:23 +0900

    initial commit

---
+++ src/main/java/jp/co/sunarch/telework/kokaon/KokaonApplication.java
@@ -0,0 +1,13 @@
+package jp.co.sunarch.telework.kokaon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokaonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KokaonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/sunarch/telework/kokaon/KokaonApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.sunarch.telework.kokaon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokaonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
