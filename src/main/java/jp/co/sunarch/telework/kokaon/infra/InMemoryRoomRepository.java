41ecd5bb2dc29f64ff90f1a1819f0a009bc3900c
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2022-03-21 00:23:12 +0900

    リポジトリの実装を追加

---
+++ src/main/java/jp/co/sunarch/telework/kokaon/infra/InMemoryRoomRepository.java
@@ -0,0 +1,34 @@
+package jp.co.sunarch.telework.kokaon.infra;
+
+import jp.co.sunarch.telework.kokaon.model.Room;
+import jp.co.sunarch.telework.kokaon.model.RoomId;
+import jp.co.sunarch.telework.kokaon.model.RoomRepository;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * ルームリポジトリのインメモリ実装。
+ *
+ * @author takeshi
+ */
+public class InMemoryRoomRepository implements RoomRepository {
+  private final Map<RoomId, Room> rooms = new ConcurrentHashMap<>();
+
+  @Override
+  public RoomId generateRoomId() {
+    return new RoomId(UUID.randomUUID().toString());
+  }
+
+  @Override
+  public void save(Room room) {
+    this.rooms.put(room.id(), room);
+  }
+
+  @Override
+  public Optional<Room> findById(RoomId roomId) {
+    return Optional.ofNullable(this.rooms.get(roomId));
+  }
+}
